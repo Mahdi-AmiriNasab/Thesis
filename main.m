2ed9b98d29ccc9607116cb82da89f428c76f1411
mahdi <mehdi.amirinasab@yahoo.com>
2023-10-26 19:20:45 +0330

    soc transfer successfully done for a single noise cell. noise_soc member needs change into cluster_noise_soc :

--- main.m
+++ main.m
@@ -1,28 +1,17 @@
 
 %% start clustering
+
 clear;
+close all
 cell_count = 9;
-soc = fix(rand (1, cell_count) * 100);
+%soc = fix(rand (1, cell_count) * 100);
+soc = [69    31    95     3    43    38    76    79    18];
 mp  = 2;
 ep = 10;
 [cluster] = pso_DBSCAN(soc, mp, ep);
 
-%% find the closest cluster to extermum niose
+%% tagging
 
-% collecting soc average of each cluster with cluster index in v
-vi = 1;
-v = [];
-for n = 1:cell_count
-    if cluster.clt_res_cell(n , 1) ~= 0
-        if cluster.clt_res_cell(n, 2) ~= 0
-            v(1, vi) = n;
-            v(2, vi) = cluster.clt_res_soc_av(n, 1);    
-            vi = vi + 1;
-        end
-    end
-end 
-
-% proposed 
 % if there is any noise 
 if cluster.noise_max(2, :) == 0x00 && cluster.noise_min(2, :) == 0x00
     noise_status = e_noise_stat.noise_not_found;
@@ -37,18 +26,221 @@ else
     cluster_status = e_cluster_stat.cluster_found;
 end
 
+%% finding the noise neighbors
+% calculation for noise_max
 
-if noise_status == e_noise_stat.noise_found        
-    % find the index of the closest soc cluster averages to the noise values
-    [~, index_x] = min(abs(cluster.clt_res_soc_av(:, 1) - cluster.noise_max(2, 1)));
-    [~, index_n] = min(abs(cluster.clt_res_soc_av(:, 1) - cluster.noise_min(2, 1)));
-    
-    nearest_valuemx_x = cluster.clt_res_soc_av(index_x);
-    nearest_valuemx_n = cluster.clt_res_soc_av(index_n);
+% sorting cluster.clt_res_soc_av 
+V = cluster.clt_res_soc_av;
+V(V(:,2)==0,2) = Inf;
+cluster.clt_res_soc_av = sortrows(V, 2,'ascend');
+cluster.clt_res_soc_av(cluster.clt_res_soc_av(:,2)==Inf,2) = 0;
+
+r = cluster.noise_max(1, 1);            % select the cluster number of the noise
+value = cluster.clt_res_soc_av(r,1);    % select the containing value
+
+% pick the neighbor values
+% watch for the zero index
+if r - 1 > 0
+    value_lower = cluster.clt_res_soc_av(r - 1,1);  
 else
-    
+    value_lower = cluster.clt_res_soc_av(r ,1);  
+end
+% watch for the maximum boundaries
+if r + 1 <= cell_count
+    value_upper = cluster.clt_res_soc_av(r + 1,1);
+else
+    value_upper = cluster.clt_res_soc_av(r ,1);
 end
 
+% differantion of the neighbor values
+diff_lower = abs(value - value_lower);
+diff_upper = abs(value - value_upper);
+
+% store closest values and its index
+neighbor_distance_x = min(diff_lower, diff_upper);
+if neighbor_distance_x == diff_lower
+    closest_cluster_x = cluster.clt_res_soc_av(r - 1, :);
+else
+    closest_cluster_x = cluster.clt_res_soc_av(r + 1, :);
+end
+
+% calculation for noise_min
+
+r = cluster.noise_min(1, 1);            % select the cluster number of the noise
+value = cluster.clt_res_soc_av(r,1);    % select the containing value
+
+% pick the neighbor values
+% watch for the zero index
+if r - 1 > 0
+    value_lower = cluster.clt_res_soc_av(r - 1,1);  
+else
+    value_lower = cluster.clt_res_soc_av(r ,1);  
+end
+% watch for the maximum boundaries
+if r + 1 <= cell_count
+    value_upper = cluster.clt_res_soc_av(r + 1,1);
+else
+    value_upper = cluster.clt_res_soc_av(r ,1);
+end
+
+% differantion of the neighbor values
+diff_lower = abs(value - value_lower);
+diff_upper = abs(value - value_upper);
+
+% store closest values and its index
+neighbor_distance_n = min(diff_lower, diff_upper);
+if neighbor_distance_n == diff_lower
+    closest_cluster_n = cluster.clt_res_soc_av(r - 1, :);
+else
+    closest_cluster_n = cluster.clt_res_soc_av(r + 1, :);
+end
+
+target_cluster_soc = [0 0]; % stores the cluster and target soc to reach
+target_transfer_soc = 0;    % stores the soc to transfer
+
+% decide the target cluster soc
+if neighbor_distance_x < neighbor_distance_n
+    target_cluster_soc =  closest_cluster_x;
+    target_transfer_soc = neighbor_distance_x;
+else
+    target_cluster_soc =  closest_cluster_n;
+    target_transfer_soc = neighbor_distance_n;
+end
+%clear temporary variables used
+clear r V value_upper value_lower diff_lower diff_upper
+
+%% transfer charges
+
+soc_new = soc;
+noise_max_new = cluster.noise_max;
+noise_min_new = cluster.noise_min;
+
+noise_max_new(2, 1) = noise_max_new(2, 1) - target_transfer_soc;
+noise_min_new(2, 1) = noise_min_new(2, 1) + target_transfer_soc;
+soc_new(1, noise_min_new(1, 1)) = noise_min_new(2, 1);
+soc_new(1, noise_max_new(1, 1)) = noise_max_new(2, 1);
+
+figure;
+mp  = 2;
+ep = 10;
+[cluster] = pso_DBSCAN(soc_new, mp, ep);
+soc = soc_new;
+
+
+
+
+
+
+
+
+
+
+%% finding the noise neighbors
+% calculation for noise_max
+
+% sorting cluster.clt_res_soc_av 
+V = cluster.clt_res_soc_av;
+V(V(:,2)==0,2) = Inf;
+cluster.clt_res_soc_av = sortrows(V, 2,'ascend');
+cluster.clt_res_soc_av(cluster.clt_res_soc_av(:,2)==Inf,2) = 0;
+
+r = cluster.noise_max(1, 1);            % select the cluster number of the noise
+value = cluster.clt_res_soc_av(r,1);    % select the containing value
+
+% pick the neighbor values
+% watch for the zero index
+if r - 1 > 0
+    value_lower = cluster.clt_res_soc_av(r - 1,1); 
+else
+    value_lower = cluster.clt_res_soc_av(r ,1);  
+end
+% watch for the maximum boundaries
+if r + 1 <= cell_count
+    value_upper = cluster.clt_res_soc_av(r + 1,1);
+else
+    value_upper = cluster.clt_res_soc_av(r ,1);
+end
+
+% differantion of the neighbor values
+diff_lower = abs(value - value_lower);
+diff_upper = abs(value - value_upper);
+
+% store closest values and its index
+neighbor_distance_x = min(diff_lower, diff_upper);
+
+% watch boundaries
+if r - 1 > 0
+    if neighbor_distance_x == diff_lower
+        closest_cluster_x = cluster.clt_res_soc_av(r - 1, :);
+    else
+        closest_cluster_x = cluster.clt_res_soc_av(r + 1, :);
+    end
+else
+    closest_cluster_x = cluster.clt_res_soc_av(r + 1, :);
+end
+% calculation for noise_min
+
+r = cluster.noise_min(1, 1);            % select the cluster number of the noise
+value = cluster.clt_res_soc_av(r,1);    % select the containing value
+
+% pick the neighbor values
+% watch for the zero index
+if r - 1 > 0
+    value_lower = cluster.clt_res_soc_av(r - 1,1);  
+else
+    value_lower = cluster.clt_res_soc_av(r ,1);  
+end
+% watch for the maximum boundaries
+if r + 1 <= cell_count
+    value_upper = cluster.clt_res_soc_av(r + 1,1);
+else
+    value_upper = cluster.clt_res_soc_av(r ,1);
+end
+
+% differantion of the neighbor values
+diff_lower = abs(value - value_lower);
+diff_upper = abs(value - value_upper);
+
+% store closest values and its index
+neighbor_distance_n = min(diff_lower, diff_upper);
+if r + 1 <= cell_count
+    if neighbor_distance_n == diff_lower
+        closest_cluster_n = cluster.clt_res_soc_av(r - 1, :);
+    else
+        closest_cluster_n = cluster.clt_res_soc_av(r + 1, :);
+    end
+else
+    closest_cluster_n = cluster.clt_res_soc_av(r - 1, :);
+end
+target_cluster_soc = [0 0]; % stores the cluster and target soc to reach
+target_transfer_soc = 0;    % stores the soc to transfer
+
+% decide the target cluster soc
+if neighbor_distance_x < neighbor_distance_n
+    target_cluster_soc =  closest_cluster_x;
+    target_transfer_soc = neighbor_distance_x;
+else
+    target_cluster_soc =  closest_cluster_n;
+    target_transfer_soc = neighbor_distance_n;
+end
+%clear temporary variables used
+clear r V value_upper value_lower diff_lower diff_upper
+
+%% transfer charges
+
+soc_new = soc;
+noise_max_new = cluster.noise_max;
+noise_min_new = cluster.noise_min;
+
+noise_max_new(2, 1) = noise_max_new(2, 1) - target_transfer_soc;
+noise_min_new(2, 1) = noise_min_new(2, 1) + target_transfer_soc;
+soc_new(1, noise_min_new(1, 1)) = noise_min_new(2, 1);
+soc_new(1, noise_max_new(1, 1)) = noise_max_new(2, 1);
+
+figure;
+mp  = 2;
+ep = 10;
+[cluster] = pso_DBSCAN(soc_new, mp, ep);
 
 
 
--- pso_DBSCAN.m
+++ pso_DBSCAN.m
@@ -95,9 +95,28 @@ else
 
 end
 
+vis = [];
+
+for i = 1:size(clt.clt_res_cell ,1)
+    nonZeroIndices = find(clt.clt_res_cell(i,:));
+    vis = [vis; repmat(i, length(nonZeroIndices), 1)];
+end
 
 %% vizualization
-clustering_v= dbscan_res_clm + 2;  % Adjust cluster indices for visualization
+clustering_v= vis + 2;  % Adjust cluster indices for visualization
+
+num_clusters = length(unique(clustering_v));  % Get the number of unique elements in clustering_v
+my_colormap = hsv(num_clusters);  % Generate a colormap with num_clusters colors
+unique_clusters = unique(clustering_v);
+color_map = containers.Map('KeyType', 'double', 'ValueType', 'any');
+for i = 1:num_clusters
+    color_map(unique_clusters(i)) = my_colormap(i, :);
+end
+color_v = zeros(length(clustering_v), 3);  % Initialize color_v
+for i = 1:length(clustering_v)
+    color_v(i, :) = color_map(clustering_v(i));
+end
+
 scatter(1:length(socs'), socs', 100, clustering_v', 'filled');
 yline(clt.average, '-', 'clt.average');
 colorbar;
