0dd4f5a19dd65c24dedcaf4fcb432086b767442b
mahdi <mehdi.amirinasab@yahoo.com>
2023-10-26 10:29:17 +0330

    first commit on thieses. transfering chrges between top or bottom clusters

---
+++ e_cluster_stat.m
@@ -0,0 +1,6 @@
+classdef e_cluster_stat < Simulink.IntEnumType
+    enumeration
+        cluster_not_found (0),
+        cluster_found (1)
+    end
+end
---
+++ e_noise_stat.m
@@ -0,0 +1,6 @@
+classdef e_noise_stat < Simulink.IntEnumType
+    enumeration
+        noise_not_found (0),
+        noise_found (1)
+    end
+end
